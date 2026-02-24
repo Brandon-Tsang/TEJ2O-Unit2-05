c2f989b7147e31942b4bc9d2d4e8a189cabb98dd
Brandon-Tsang <brandon.tsang@stu.ocsb.ca>
2026-02-24 13:05:47 -0500

    Tell me the temperature in kelvin.

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Brandon
+ * Created on: Feb 2026
+ * This program will tell me the temperature in kelvin.
 */
 
-basic.showString('Hello, World!')
+let currentTemperature: number
+let lightValuePercentage: number
+
+basic.clearScreen ()
+basic.showIcon (IconNames.Happy)
+
+    input.onButtonPressed(Button.A, function () {
+        const currentTemperature = input.temperature();
+        basic.showString("The Temperature is: " + convertToText (Math.round(currentTemperature + 273.15)) + " K.")
+    })
